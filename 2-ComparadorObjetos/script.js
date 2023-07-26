b031c6c98481cdee39695436f5593a0c01865b9d
Carlos Rico <ricgarcar@gmail.com>
2023-07-26 09:32:40 +0200

    iniciamos carpeta ejercicios

---
+++ 2-ComparadorObjetos/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+//EJERCICIO
+//Crea una función que reciba dos objetos y devuelva true si son iguales y false si no.
+//En los ejemplos, son iguales los dos primeros
+
+const obj1 = {
+  key1: "a",
+  key2: "b",
+};
+
+const obj2 = {
+  key2: "b",
+  key1: "a",
+};
+
+const obj3 = {
+  key1: "a",
+  key2: "c",
+};
+
+const obj4 = {
+  key1: "a",
+  key2: "b",
+  key3: "c",
+};
+
+const obj5 = {
+  key1: "a",
+  key3: "b",
+};
+
+function sonIguales(objA, objB) {}
+
+console.log(sonIguales(obj1, obj2)); //true
+console.log(sonIguales(obj1, obj3)); //false
+console.log(sonIguales(obj1, obj4)); //false
+console.log(sonIguales(obj1, obj5)); //false

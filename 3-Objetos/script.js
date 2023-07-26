27313373c0e2bdc7f45be69269334d92dbe8924d
Carlos Rico <ricgarcar@gmail.com>
2023-07-26 10:50:36 +0200

    contar animales realizado

--- 3-Objetos/script.js
+++ 3-Objetos/script.js
@@ -40,3 +40,41 @@ const animals = [
     species: "cat",
   },
 ];
+
+//Escribe una función que, al recibir un array con la forma del ejemplo a continuación, devuelva un objeto con el número de animales de cada especie. Piensa que el array puede ser modificado para añadir nuevas especies que no estuviesen en el original.
+
+function animales(valor){
+  let nuevoObjeto = {}
+  let contadorDog = 0
+  let contadorCat = 0
+  let contadorChicken = 0
+  let contadorSnake = 0
+  for (let objetos of valor){
+    // console.log(objetos.name, objetos.species)
+
+    if (objetos.species === "dog"){
+      contadorDog++
+      const resultado = contadorDog
+        nuevoObjeto["dog"] = resultado
+    
+        
+    }if(objetos.species === "cat"){
+      contadorCat ++
+      const resultado1 = contadorCat
+      nuevoObjeto["cat"] = resultado1
+    }if(objetos.species === "chicken"){
+      contadorChicken ++
+      const resultado2 = contadorChicken
+      nuevoObjeto["chicken"] = resultado2
+    }if(objetos.species === "snake"){
+      contadorSnake ++
+      const resultado3 = contadorSnake
+      nuevoObjeto["snake"] = resultado3
+    }
+  }
+
+  return nuevoObjeto
+}
+
+const contar = animales(animals)
+console.log(contar)
